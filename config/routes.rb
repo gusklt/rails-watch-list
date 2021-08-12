8876ab0cd84826dc22a2329d98f7a053495f71ba
Augustin Clot <augustin.clot@gmail.com>
2021-08-12 17:17:27 +0200

    beginnig of the new list route and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'lists/new', to: 'lists#new'
+  get 'lists/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show', as: :list
 end
