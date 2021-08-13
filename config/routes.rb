09aec771d9761c80132cbb607da5abe0c1896925
Augustin Clot <augustin.clot@gmail.com>
2021-08-13 17:04:25 +0200

    fffff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  get 'lists/new', to: 'lists#new', as: :new_list
-  post 'lists', to: 'lists#create'
-  get 'lists/:id', to: 'lists#show', as: :list
+  get '/lists/new', to: 'lists#new', as: :new_list
+  post '/lists', to: 'lists#create'
+  get '/lists/:id', to: 'lists#show', as: :list
 end
