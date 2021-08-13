1c3b4d8481d19b9aa3c45282a0afceaf82e32415
Augustin Clot <augustin.clot@gmail.com>
2021-08-13 16:50:10 +0200

    heroku push

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,9 +9,22 @@ class ListsController < ApplicationController
   end
 
   def new
-
+    @list = List.new
   end
 
   def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'lists/new', to: 'lists#new'
-  get 'lists/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'lists#index', as: :lists
+  root to: 'lists#index'
+  get 'lists/new', to: 'lists#new', as: :new_list
+  post 'lists', to: 'lists#create'
   get 'lists/:id', to: 'lists#show', as: :list
 end
